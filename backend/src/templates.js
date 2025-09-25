89e0d7e5cc25fb19fc9847581e281a09948e52db
Matthew Greathouse <132587783+c0deirl@users.noreply.github.com>
2025-09-25 14:22:34 -0400

    Delete backend directory

--- backend/src/templates.js
+++
@@ -1,8 +0,0 @@
-const Handlebars = require('handlebars');
-
-function render(template, context) {
-  const tpl = Handlebars.compile(template || '');
-  return tpl(context);
-}
-
-module.exports = { render };
